0e47d7a4f9fd7d488cd51d912ac6078223f46b81
Sam Lazrak <24284329+samlazrak@users.noreply.github.com>
2018-06-03 23:14:21 -0500

    Added comments to help explain changes

--- aircon.ino
+++ aircon.ino
@@ -1,7 +1,9 @@
+// Imports from C++ library to convert string to int and check if int
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+// Environment variables
 int PowerAcPIN = D1;
 int tempUpPIN = D2;
 int tempDownPIN = D3;
@@ -10,14 +12,17 @@ int degrees;
 void setup()
 {
 
+  // Setting up pins for use
   pinMode(PowerAcPIN, OUTPUT);
   pinMode(tempUpPIN, OUTPUT);
   pinMode(tempDownPIN, OUTPUT);
 
+  // Setting pins to low
   digitalWrite(PowerAcPIN, LOW);
   digitalWrite(tempUpPIN, LOW);
   digitalWrite(tempDownPIN, LOW);
 
+  // Particle IFTTT functions
   Particle.function("PowerAc", PowerAc);
   Particle.function("tempUp", tempUp);
   Particle.function("tempDown", tempDown);
@@ -27,6 +32,7 @@ void loop()
 {
 }
 
+// Gets String of command and checks if it matches power, if it does it toggles the AC.
 int PowerAc(String command)
 {
   if (command == "power")
@@ -40,13 +46,17 @@ int PowerAc(String command)
     return -1;
 }
 
+// Gets String of command from IFTTT, string will be # of degrees to increase.
 int tempUp(String command)
 {
+  // Uses atoi to convert it to an int by assigning to the degrees variable that's an int
   degrees = atoi(command);
 
+  // Uses ctype library to check if degrees is a number with the isdigit function
   if (isdigit(degrees))
   {
 
+    // Loops through the requested number of times, might need a delay.
     for (int i = 0; i < degrees; ++i)
     {
       digitalWrite(tempUpPIN, HIGH);
@@ -59,13 +69,17 @@ int tempUp(String command)
     return -1;
 }
 
+// Gets String of command from IFTTT, string will be # of degrees to decrease.
 int tempDown(String command)
 {
+  // Uses atoi to convert it to an int by assigning to the degrees variable that's an int
   degrees = atoi(command);
 
+  // Uses ctype library to check if degrees is a number with the isdigit function
   if (isdigit(degrees))
   {
 
+    // Loops through the requested number of times, might need a delay.
     for (int i = 0; i < degrees; ++i)
     {
       digitalWrite(tempDownPIN, HIGH);
